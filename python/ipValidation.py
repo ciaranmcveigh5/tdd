b1a90db73ab916fdcf1d7add17beeee762639b50
Ciaran McVeigh <cimcveigh@deloitte.co.uk>
2017-09-13 12:42:48 +0100

    updated test cases

--- python/ipValidation.py
+++ python/ipValidation.py
@@ -47,34 +47,50 @@ def isOctetMadeOfCharacters(octet):
 
 
 
-
-
-
 class TestIpValidation(unittest.TestCase):
 
     def test_empty_ip(self):
-        self.assertEqual(isIpValid(''), False)
+        self.assertFalse(isIpValid(''))
 
     def test_valid_ip(self):
-        self.assertEqual(isIpValid('1.2.3.4'), True)
+        self.assertTrue(isIpValid('1.2.3.4'))
+
+    def test_ip_split_into_octets(self):
+        self.assertEqual(splitIpIntoOctets('1.2.3.4'), ['1', '2', '3', '4'])
+
+    def test_octet_valid(self):
+        self.assertFalse(isOctetValidValue(['-1']))
+        self.assertFalse(isOctetValidValue(['256']))
+        self.assertTrue(isOctetValidValue(['1', '255']))
 
     def test_octet_over_255_range(self):
-        self.assertEqual(isIpValid('1.2.3.256'), False)
+        self.assertFalse(isIpValid('1.2.3.256'))
 
     def test_octet_below_0_range(self):
-        self.assertEqual(isIpValid('1.2.-3.255'), False)
+        self.assertFalse(isIpValid('1.2.-3.255'))
+
+    def test_is_octet_made_of_string_chars(self):
+        self.assertTrue(isOctetMadeOfCharacters('65i'))
+        self.assertTrue(isOctetMadeOfCharacters('h'))
+        self.assertTrue(isOctetMadeOfCharacters('65>'))
+        self.assertFalse(isOctetMadeOfCharacters("67"))
 
     def test_octet_is_string(self):
-        self.assertEqual(isIpValid('1.2.i.hello'), False)
+        self.assertFalse(isIpValid('1.2.i.hello'))
 
     def test_over_valid_ip_length(self):
-        self.assertEqual(isIpValid('1.2.3.4.5'), False)
+        self.assertFalse(isIpValid('1.2.3.4.5'))
 
     def test_under_valid_ip_length(self):
-        self.assertEqual(isIpValid('1.2.3'), False)
+        self.assertFalse(isIpValid('1.2.3'))
 
     def test_space_in_octet(self):
-        self.assertEqual(isIpValid('1.2 .3.5'), False)
+        self.assertFalse(isIpValid('1.2 .3.5'))
+
+    def test_leading_zero(self):
+        self.assertTrue(isLeadingZeroInOctet('01'))
+        self.assertTrue(isLeadingZeroInOctet('001'))
+        self.assertFalse(isLeadingZeroInOctet('1'))
 
     def test_leading_zero_in_octet(self):
-        self.assertEqual(isIpValid('1.02.3.4'), False)
+        self.assertFalse(isIpValid('1.02.3.4'))
